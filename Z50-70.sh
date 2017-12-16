d92a43c9a1f7e223bfaf38bd0c2d3e576bcd0042
Zaid Rahawi <zaid@Zaids-macOS.local>
2017-12-16 14:59:57 +0300

    Minor changes to Z50-70.sh

--- Z50-70.sh
+++ Z50-70.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-hda_resources=Resources_CX20751
-
-EFI=$(macos-tools/mount_efi.sh)
-
 function downloadTools() {
     echo "Downloading macos-tools..."
     rm -Rf macos-tools && git clone https://github.com/the-braveknight/macos-tools --quiet
@@ -23,7 +19,7 @@ function installPS2Kext() {
 
 function installHDAInjector() {
     # Create XML/Layout files injector; PinConfigs are injected with CodecCommander.kext & SSDT-HDEF
-    macos-tools/create_xmlinjector.sh $hda_resources
+    macos-tools/create_xmlinjector.sh Resources_CX20751
     macos-tools/install_kext.sh AppleHDAInjector.kext
 }
 
@@ -31,7 +27,7 @@ function installBacklightInjector() {
     macos-tools/install_kext.sh Kexts/AppleBacklightInjector.kext
 }
 
-function installKexts() {
+function installDownloads() {
     installHDAInjector
     installPS2Kext
     installBacklightInjector
@@ -58,34 +54,40 @@ function compileACPI() {
 }
 
 function installACPI() {
+    EFI=$(macos-tools/mount_efi.sh)
     rm -f $EFI/EFI/Clover/ACPI/patched/*.aml
     macos-tools/install_acpi.sh Build/*.aml
 }
-
-if [[ ! -d macos-tools ]]; then
-    downloadTools
-fi
 
 case "$1" in
     --download-tools)
         downloadTools
         ;;
-    --download-requirements)
-        downloadRequirements
-        ;;
-    --install-kexts)
-        installKexts
-        ;;
-    --install-config)
-        installConfig
-        ;;
-    --update-config)
-        updateConfig
-        ;;
-    --compile-acpi)
-        compileACPI "$2"
-        ;;
-    --install-acpi)
-        installACPI
-        ;;
+    *)
+        if [[ ! -d macos-tools ]]; then
+            echo "macos-tools not downloaded."
+            echo "Use --download-tools argument."
+            exit 1
+        fi
+        case "$1" in
+            --download-requirements)
+                downloadRequirements
+                ;;
+            --install-downloads)
+                installDownloads
+                ;;
+            --install-config)
+                installConfig
+                ;;
+            --update-config)
+                updateConfig
+                ;;
+            --compile-acpi)
+                compileACPI "$2"
+                ;;
+            --install-acpi)
+                installACPI
+                ;;
+        esac
 esac
+
