b432669dc3983c4022c58f3c3bdced0712ee06c9
Zaid Rahawi <zaid.riadh@gmail.com>
2018-01-05 13:43:37 +0300

    Refactor code in X50.sh

--- X50.sh
+++ X50.sh
@@ -5,70 +5,68 @@ function downloadTools() {
     rm -Rf macos-tools && git clone https://github.com/the-braveknight/macos-tools --quiet
 }
 
-function downloadRequirements() {
-    rm -Rf Downloads/Kexts && mkdir Downloads/Kexts && macos-tools/bitbucket_download.sh -p Downloads/Kexts.plist -o Downloads/Kexts
-    rm -Rf Downloads/Tools && mkdir Downloads/Tools && macos-tools/bitbucket_download.sh -p Downloads/Tools.plist -o Downloads/Tools
-    rm -Rf Downloads/Hotpatch && mkdir Downloads/Hotpatch && macos-tools/hotpatch_download.sh -p Downloads/Hotpatch.plist -o Downloads/Hotpatch
-}
-
-function installPS2Kext() {
-    if [[ "$(macos-tools/trackpad_model.sh)" == *"SYN"* ]]; then
-        macos-tools/install_kext.sh $(macos-tools/find_kext.sh VoodooPS2Controller.kext)
-        sudo rm -Rf /Library/Extensions/ApplePS2SmartTouchPad.kext
-    else
-        macos-tools/install_kext.sh Kexts/ApplePS2SmartTouchPad.kext
-        sudo rm -Rf /Library/Extensions/VoodooPS2Controller.kext
-    fi
-}
-
-function installHDAInjector() {
-    macos-tools/create_hdainjector.sh -c CX20751 -r Resources_CX20751
-    macos-tools/install_kext.sh AppleHDA_CX20751.kext
-}
-
-function installBacklightInjector() {
-    macos-tools/install_kext.sh Kexts/AppleBacklightInjector.kext
-}
-
-function installDownloads() {
-    macos-tools/unarchive_file.sh -d Downloads
-    macos-tools/install_binary.sh -d Downloads
-    macos-tools/install_app.sh -d Downloads
-    macos-tools/install_kext.sh -d Downloads -e Kext-Exceptions.plist
-
-    installHDAInjector
-    installPS2Kext
-    installBacklightInjector
-
-    sudo kextcache -i /
-}
-
-function installConfig() {
-    macos-tools/install_config.sh config.plist
-}
-
-function updateConfig() {
-    macos-tools/update_config.sh config.plist
-}
+if [[ ! -d macos-tools ]]; then downloadTools; fi
 
 case "$1" in
     --download-tools)
-        downloadTools
+        rm -Rf Downloads/Tools && mkdir Downloads/Tools && macos-tools/bitbucket_download.sh -p Downloads/Tools.plist -o Downloads/Tools
     ;;
-    *)
-    if [[ ! -d macos-tools ]]; then downloadTools; fi
-    case "$1" in
-        --download-requirements)
-            downloadRequirements
-        ;;
-        --install-downloads)
-            installDownloads
-        ;;
-        --install-config)
-            installConfig
-        ;;
-        --update-config)
-            updateConfig
-        ;;
-    esac
+    --download-kexts)
+        rm -Rf Downloads/Kexts && mkdir Downloads/Kexts && macos-tools/bitbucket_download.sh -p Downloads/Kexts.plist -o Downloads/Kexts
+    ;;
+    --download-hotpatch)
+        rm -Rf Downloads/Hotpatch && mkdir Downloads/Hotpatch && macos-tools/hotpatch_download.sh -p Downloads/Hotpatch.plist -o Downloads/Hotpatch
+    ;;
+    --unarchive-downloads)
+        macos-tools/unarchive_file.sh -d Downloads
+    ;;
+    --install-apps)
+        macos-tools/install_app.sh -d Downloads
+    ;;
+    --install-binaries)
+        macos-tools/install_binary.sh -d Downloads
+    ;;
+    --install-kexts)
+        macos-tools/install_kext.sh -d Downloads -e Kext-Exceptions.plist
+    ;;
+    --install-hdainjector)
+        macos-tools/create_hdainjector.sh -c CX20751 -r Resources_CX20751
+        macos-tools/install_kext.sh AppleHDA_CX20751.kext
+    ;;
+    --install-backlightinjector)
+        macos-tools/install_kext.sh Kexts/AppleBacklightInjector.kext
+    ;;
+    --install-ps2kext)
+        if [[ "$(macos-tools/trackpad_model.sh)" == *"SYN"* ]]; then
+            macos-tools/install_kext.sh $(macos-tools/find_kext.sh VoodooPS2Controller.kext)
+            sudo rm -Rf /Library/Extensions/ApplePS2SmartTouchPad.kext
+        else
+            macos-tools/install_kext.sh Kexts/ApplePS2SmartTouchPad.kext
+            sudo rm -Rf /Library/Extensions/VoodooPS2Controller.kext
+        fi
+    ;;
+    --update-kernelcache)
+        sudo kextcache -i /
+    ;;
+    --install-config)
+        macos-tools/install_config.sh config.plist
+    ;;
+    --update-config)
+        macos-tools/update_config.sh config.plist
+    ;;
+    --download-requirements)
+        $0 --download-kexts
+        $0 --download-tools
+        $0 --download-hotpatch
+    ;;
+    --install-downloads)
+        $0 --unarchive-downloads
+        $0 --install-binaries
+        $0 --install-apps
+        $0 --install-kexts
+        $0 --install-backlightinjector
+        $0 --install-hdainjector
+        $0 --install-ps2kext
+        $0 --update-kernelcache
+    ;;
 esac
